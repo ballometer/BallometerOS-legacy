615eb80b9afe520b69c72c7a0e217327a59bd8a0
wipfli <mail@example.com>
2020-08-02 18:47:31 +0200

    Add python packages python-rplcd and python-statemachine

---
+++ package/python-statemachine/python-statemachine.mk
@@ -0,0 +1,13 @@
+################################################################################
+#
+# python-statemachine
+#
+################################################################################
+
+PYTHON_STATEMACHINE_VERSION = 0.8.0
+PYTHON_STATEMACHINE_SITE = https://files.pythonhosted.org/packages/f4/e5/6b848e23ef075c38c81c9c094decb38a00338db6d4b55a50e0b3017073c8
+PYTHON_STATEMACHINE_SETUP_TYPE = setuptools
+PYTHON_STATEMACHINE_LICENSE = MIT
+PYTHON_STATEMACHINE_LICENSE_FILES = LICENSE
+
+$(eval $(python-package))
