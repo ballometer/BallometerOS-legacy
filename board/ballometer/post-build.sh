5aa787e067912a7cb8ed096d4fc886c9da53b999
wipfli <mail@example.com>
2020-08-21 21:45:15 +0200

    Move wifi files from post-build.sh to overlay

--- board/ballometer/post-build.sh
+++ board/ballometer/post-build.sh
@@ -10,17 +10,7 @@ if [ -e ${TARGET_DIR}/etc/inittab ]; then
 tty1::respawn:/sbin/getty -L  tty1 0 vt100 # HDMI console' ${TARGET_DIR}/etc/inittab
 fi
 
-# for wifi
-cp package/busybox/S10mdev ${TARGET_DIR}/etc/init.d/S10mdev
-chmod 755 ${TARGET_DIR}/etc/init.d/S10mdev
-cp package/busybox/mdev.conf ${TARGET_DIR}/etc/mdev.conf
- 
-cp board/ballometer/interfaces ${TARGET_DIR}/etc/network/interfaces
-cp board/ballometer/wpa_supplicant.conf ${TARGET_DIR}/etc/wpa_supplicant.conf
-cp board/ballometer/sshd_config ${TARGET_DIR}/etc/ssh/sshd_config
-
 cp board/ballometer/cmdline-p2.txt output/images/rpi-firmware/cmdline-p2.txt
 cp board/ballometer/cmdline-p3.txt output/images/rpi-firmware/cmdline-p3.txt
 cp board/ballometer/config.txt output/images/rpi-firmware/config.txt
 cp board/ballometer/select.txt output/images/rpi-firmware/select.txt
-
