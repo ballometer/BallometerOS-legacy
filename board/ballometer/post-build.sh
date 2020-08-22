3dbc01e1436dd9c9f7c806f86794b59c9fcf27d8
wipfli <mail@example.com>
2020-08-22 22:43:20 +0200

    Create data.ext2 partition

--- board/ballometer/post-build.sh
+++ board/ballometer/post-build.sh
@@ -7,3 +7,7 @@ cp board/ballometer/boot/cmdline-p2.txt output/images/rpi-firmware/cmdline-p2.tx
 cp board/ballometer/boot/cmdline-p3.txt output/images/rpi-firmware/cmdline-p3.txt
 cp board/ballometer/boot/config.txt output/images/rpi-firmware/config.txt
 cp board/ballometer/boot/select.txt output/images/rpi-firmware/select.txt
+
+rm output/images/data.ext4 output/images/data.ext2
+output/host/sbin/mkfs.ext4 -d board/ballometer/data -r 1 -N 0 -m 5 -L "data" -O ^64bit output/images/data.ext2 "100M"
+ln -sf data.ext2 output/images/data.ext4
