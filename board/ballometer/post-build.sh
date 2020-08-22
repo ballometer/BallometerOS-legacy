3cb742c277035eeeb5fcea3360d84299db799839
wipfli <mail@example.com>
2020-08-22 23:23:44 +0200

    Replace output with  in post-build.sh

--- board/ballometer/post-build.sh
+++ board/ballometer/post-build.sh
@@ -3,11 +3,11 @@
 set -u
 set -e
 
-cp board/ballometer/boot/cmdline-p2.txt output/images/rpi-firmware/cmdline-p2.txt
-cp board/ballometer/boot/cmdline-p3.txt output/images/rpi-firmware/cmdline-p3.txt
-cp board/ballometer/boot/config.txt output/images/rpi-firmware/config.txt
-cp board/ballometer/boot/select.txt output/images/rpi-firmware/select.txt
+cp board/ballometer/boot/cmdline-p2.txt ${O}/images/rpi-firmware/cmdline-p2.txt
+cp board/ballometer/boot/cmdline-p3.txt ${O}/images/rpi-firmware/cmdline-p3.txt
+cp board/ballometer/boot/config.txt ${O}/images/rpi-firmware/config.txt
+cp board/ballometer/boot/select.txt ${O}/images/rpi-firmware/select.txt
 
-rm output/images/data.ext4 output/images/data.ext2
-output/host/sbin/mkfs.ext4 -d board/ballometer/data -r 1 -N 0 -m 5 -L "data" -O ^64bit output/images/data.ext2 "100M"
-ln -sf data.ext2 output/images/data.ext4
+rm ${O}/images/data.ext4 ${O}/images/data.ext2
+${O}/host/sbin/mkfs.ext4 -d board/ballometer/data -r 1 -N 0 -m 5 -L "data" -O ^64bit ${O}/images/data.ext2 "100M"
+ln -sf data.ext2 ${O}/images/data.ext4
