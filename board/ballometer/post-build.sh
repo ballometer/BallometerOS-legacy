f12a74dfdafdfb54ad2121900e1a51694e9648d0
wipfli <mail@example.com>
2020-08-22 21:52:09 +0200

    Remove getty tty1 duplicate config in post-build.sh

--- board/ballometer/post-build.sh
+++ board/ballometer/post-build.sh
@@ -3,13 +3,6 @@
 set -u
 set -e
 
-# Add a console on tty1
-if [ -e ${TARGET_DIR}/etc/inittab ]; then
-    grep -qE '^tty1::' ${TARGET_DIR}/etc/inittab || \
-	sed -i '/GENERIC_SERIAL/a\
-tty1::respawn:/sbin/getty -L  tty1 0 vt100 # HDMI console' ${TARGET_DIR}/etc/inittab
-fi
-
 cp board/ballometer/boot/cmdline-p2.txt output/images/rpi-firmware/cmdline-p2.txt
 cp board/ballometer/boot/cmdline-p3.txt output/images/rpi-firmware/cmdline-p3.txt
 cp board/ballometer/boot/config.txt output/images/rpi-firmware/config.txt
