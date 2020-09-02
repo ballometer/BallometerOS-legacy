9bdabb3da404555ba762d7b99940b8d2bd9bc0d2
wipfli <mail@example.com>
2020-09-02 13:57:52 +0200

    Force remove of existing data.ext4

--- board/ballometer/post-build.sh
+++ board/ballometer/post-build.sh
@@ -8,6 +8,6 @@ cp board/ballometer/boot/cmdline-p3.txt ${O}/images/rpi-firmware/cmdline-p3.txt
 cp board/ballometer/boot/config.txt ${O}/images/rpi-firmware/config.txt
 cp board/ballometer/boot/select.txt ${O}/images/rpi-firmware/select.txt
 
-rm ${O}/images/data.ext4 ${O}/images/data.ext2
+rm -f ${O}/images/data.ext4 ${O}/images/data.ext2
 ${O}/host/sbin/mkfs.ext4 -d board/ballometer/data -r 1 -N 0 -m 5 -L "data" -O ^64bit ${O}/images/data.ext2 "100M"
 ln -sf data.ext2 ${O}/images/data.ext4
