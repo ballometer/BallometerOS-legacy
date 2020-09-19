567e930f547829299250f31d4c9326ddc2d42103
wipfli <mail@example.com>
2020-09-19 08:19:50 +0200

    Exclude adafruit_blinka from architecture consistency checking to close issue #3

--- package/python-adafruit-blinka/python-adafruit-blinka.mk
+++ package/python-adafruit-blinka/python-adafruit-blinka.mk
@@ -11,6 +11,10 @@ PYTHON_ADAFRUIT_BLINKA_SETUP_TYPE = setuptools
 PYTHON_ADAFRUIT_BLINKA_LICENSE = MIT
 PYTHON_ADAFRUIT_BLINKA_LICENSE_FILES = LICENSE
 
+# Since the binary file adafruit_blinka/microcontroller/bcm283x/pulseio/libgpiod_pulsein
+# is 32 bit but the architecture is 64 bit we turn of architecture consitency checking
+PYTHON_ADAFRUIT_BLINKA_BIN_ARCH_EXCLUDE = /usr/lib/python3.8/site-packages/adafruit_blinka
+
 define PYTHON_ADAFRUIT_BLINKA_PRE_PATCH_FIXUP
 	echo "patch adafruit blinka to support software i2c on ports 24, 23"
 	cp $(PYTHON_ADAFRUIT_BLINKA_PKGDIR)/pin.py $(O)/build/python-adafruit-blinka-5.2.3/src/adafruit_blinka/microcontroller/bcm283x/pin.py
