d110995f2e6d9c465a25b053d3c08bb6a13583aa
wipfli <mail@example.com>
2020-09-20 12:42:30 +0200

    Remove root/ballometer before re-copying files

--- package/ballometer/ballometer.mk
+++ package/ballometer/ballometer.mk
@@ -10,6 +10,7 @@ BALLOMETER_SITE = https://github.com/wipfli/ballometer/archive
 BALLOMETER_LICENSE = MIT
 
 define BALLOMETER_INSTALL_TARGET_CMDS
+	rm -rf $(TARGET_DIR)/root/ballometer
 	mkdir $(TARGET_DIR)/root/ballometer
 	cp -Rp $(@D)/* $(TARGET_DIR)/root/ballometer
 endef
