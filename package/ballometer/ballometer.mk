db843237daea45f7257d85f666e83deba660d1ca
wipfli <mail@example.com>
2020-09-26 13:34:30 +0200

    Bump package/ballometer to v1.0.3

--- package/ballometer/ballometer.mk
+++ package/ballometer/ballometer.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-BALLOMETER_VERSION = 1.0.2
+BALLOMETER_VERSION = 1.0.3
 BALLOMETER_SOURCE = v$(BALLOMETER_VERSION).tar.gz
 BALLOMETER_SITE = https://github.com/wipfli/ballometer/archive
 BALLOMETER_LICENSE = MIT
