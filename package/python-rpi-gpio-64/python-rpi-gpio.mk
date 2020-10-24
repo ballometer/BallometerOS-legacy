92167defd681aff1f38a41719c7840d7c9f6e802
wipfli <mail@example.com>
2020-10-24 20:19:33 +0200

    Make python-rpi-gpio-64 not depend on BR_ARM

--- package/python-rpi-gpio-64/python-rpi-gpio.mk
+++ package/python-rpi-gpio-64/python-rpi-gpio.mk
@@ -1,14 +1,14 @@
 ################################################################################
 #
-# python-rpi-gpio
+# python-rpi-gpio-64
 #
 ################################################################################
 
-PYTHON_RPI_GPIO_VERSION = 0.7.0
-PYTHON_RPI_GPIO_SOURCE = RPi.GPIO-$(PYTHON_RPI_GPIO_VERSION).tar.gz
-PYTHON_RPI_GPIO_SITE = https://sourceforge.net/projects/raspberry-gpio-python/files
-PYTHON_RPI_GPIO_LICENSE = MIT
-PYTHON_RPI_GPIO_LICENSE_FILES = LICENCE.txt
-PYTHON_RPI_GPIO_SETUP_TYPE = distutils
+PYTHON_RPI_GPIO_64_VERSION = 0.7.0
+PYTHON_RPI_GPIO_64_SOURCE = RPi.GPIO-$(PYTHON_RPI_GPIO_64_VERSION).tar.gz
+PYTHON_RPI_GPIO_64_SITE = https://sourceforge.net/projects/raspberry-gpio-python/files
+PYTHON_RPI_GPIO_64_LICENSE = MIT
+PYTHON_RPI_GPIO_64_LICENSE_FILES = LICENCE.txt
+PYTHON_RPI_GPIO_64_SETUP_TYPE = distutils
 
 $(eval $(python-package))
